48eb3ae8007c5c57d4395ce2a829f3b961b67edc
Jojhan Silva <silvajojhan@gmail.com>
2023-08-14 20:05:36 -0500

    added typedefs for the queries and mutations

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -26,7 +26,22 @@ input BookInput {
         image: String
         link: String
     }
+    
+type Auth {
+    token: ID!
+    user: User
+}
 
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email:String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+}
 `;
 
 module.exports = typeDefs;
